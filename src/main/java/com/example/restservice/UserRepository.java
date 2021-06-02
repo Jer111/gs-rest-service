f7c0b82647bc062238d7ff9d47eb5546e46a7005
BuildTools <unconfigured@null.spigotmc.org>
2021-06-02 13:36:37 +0200

    group

--- src/main/java/com/example/restservice/UserRepository.java
+++ src/main/java/com/example/restservice/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;  
 
 public interface UserRepository extends CrudRepository<User, Long>{
-	List<User> findByGroupName( final String name );
+	List<User> findByName( final String name );
 }
